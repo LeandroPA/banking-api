053908ba53d42125f35f565343b4f3d52a748fc2
Leandro Pereira <leandroalencar@unigranrio.br>
2023-01-15 15:59:41 -0300

    General clean + dotenv

--- dock-client-api/src/app.js
+++ dock-client-api/src/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/person');
@@ -10,10 +11,9 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-require('./database')
+require('./database');
 
 app.use('/', indexRouter);
 app.use('/person', usersRouter);
