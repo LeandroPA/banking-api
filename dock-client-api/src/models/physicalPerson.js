cedcf669064aa6ff6df7e38fdc163ccad168d43f
Leandro Pereira <leandroalencar@unigranrio.br>
2023-01-15 18:29:04 -0300

    Fixing validation messages

--- dock-client-api/src/models/physicalPerson.js
+++ dock-client-api/src/models/physicalPerson.js
@@ -5,12 +5,12 @@ const { cpf } = require('cpf-cnpj-validator');
 const physicalPersonSchema = new Schema({
 	fullname: {
 		type: String,
-		required: true
+		required: [true, 'fullname is required']
 	},
 	documentNumber: {
 		type: String,
-		required: true,
-		validate: [cpf.isValid, 'Invalid document number format']
+		required: [true, 'documentNumber is required'],
+		validate: [cpf.isValid, 'Invalid documentNumber format']
 	},
 })
 
