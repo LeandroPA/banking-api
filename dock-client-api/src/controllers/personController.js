9d3745180a0b58c3945e1f8c2787a8aa6b6b11fc
Leandro Pereira <leandroalencar@unigranrio.br>
2023-01-16 17:49:17 -0300

    Fixing controller responses

--- dock-client-api/src/controllers/personController.js
+++ dock-client-api/src/controllers/personController.js
@@ -6,26 +6,26 @@ const handleResourceResponse = (res, data) =>
 exports.create = (req, res, next) => {
 
 	personService.createPerson(req.body)
-		.then(data => res.status(201).send(data))
+		.then(person => res.status(201).send(person))
 		.catch(next);
 }
 exports.get = (req, res, next) => {
 
 	personService.getPerson(req.params.id)
-		.then(person => handleResourceResponse(res, data))
+		.then(person => handleResourceResponse(res, person))
 		.catch(next);
 }
 
 exports.getByDocumentNumber = (req, res, next) => {
 
 	personService.getPersonByDocumentNumber(req.params.documentNumber)
-		.then(person => handleResourceResponse(res, data))
+		.then(person => handleResourceResponse(res, person))
 		.catch(next);
 }
 
 exports.delete = (req, res, next) => {
 
 	personService.deletePerson(req.params.id)
-		.then(person => handleResourceResponse(res, data))
+		.then(person => handleResourceResponse(res, person))
 		.catch(next);
 }
