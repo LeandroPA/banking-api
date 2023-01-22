546da233eccdb8c05e73d5dea9be23c0a360704c
Leandro Pereira <leandroalencar@unigranrio.br>
2023-01-22 00:21:01 -0300

    Inserting request param validation for account and transaction

--- dock-account-api/src/routes/account.js
+++ dock-account-api/src/routes/account.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/accountController');
+const { param } = require('express-validator');
+const validate = require('../errorHandler/requestValidationErrorHandler');
 
 router.post('/', accountController.create);
-router.get('/:id', accountController.get);
-router.post('/:id/block', accountController.block);
-router.delete('/:id/block', accountController.unblock);
-router.post('/:id/disable', accountController.disable);
-router.delete('/:id', accountController.delete);
+router.get('/:id', validate(param('id').isMongoId()), accountController.get);
+router.post('/:id/block', validate(param('id').isMongoId()), accountController.block);
+router.delete('/:id/block', validate(param('id').isMongoId()), accountController.unblock);
+router.post('/:id/disable', validate(param('id').isMongoId()), accountController.disable);
+router.delete('/:id', validate(param('id').isMongoId()), accountController.delete);
 
 module.exports = router;
--- dock-transaction-api/src/routes/transaction.js
+++ dock-transaction-api/src/routes/transaction.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const transactionController = require('../controllers/transactionController');
+const { param } = require('express-validator');
+const validate = require('../errorHandler/requestValidationErrorHandler');
 
 router.post('/deposit', transactionController.deposit);
 router.post('/withdraw', transactionController.withdraw);
-router.get('/:id', transactionController.get);
-router.get('/account/:id/balance', transactionController.getBalance);
-router.get('/account/:id/statement', transactionController.getStatement);
-router.delete('/:id', transactionController.delete);
+router.get('/:id', validate(param('id').isMongoId()), transactionController.get);
+router.get('/account/:id/balance', validate(param('id').isMongoId()), transactionController.getBalance);
+router.get('/account/:id/statement', validate(param('id').isMongoId()), transactionController.getStatement);
+router.delete('/:id', validate(param('id').isMongoId()), transactionController.delete);
 
 module.exports = router;
