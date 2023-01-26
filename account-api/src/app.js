77a78c8c3b88c673c64398ea3edd22e69eb92899
Leandro Pereira <leandroalencar@unigranrio.br>
2023-01-26 09:11:14 -0300

    Ignoring snyk csrf protection warning
    since none of the apps use cookies or sessions

--- account-api/src/app.js
+++ account-api/src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const logger = require('morgan');
 const helmet = require('helmet');
-const csurf = require('csurf');
 require('dotenv').config();
 
 const routes = require('./routes/routes');
 const errorHandlers = require('./errorHandler/errorHandler');
 
+// deepcode ignore UseCsurfForExpress: The app doesn't use cookies nor sessions.
 const app = express();
 
 app.use(logger('dev'));
@@ -15,7 +15,6 @@ app.use(express.json());
 app.use(routes);
 app.use(errorHandlers);
 app.use(helmet());
-app.use(csurf());
 
 require('./database');
 
