173ce44d6161975e948752af58fefa829d931195
Leandro Pereira <leandroalencar@unigranrio.br>
2023-01-25 23:30:19 -0300

    WIP - Creating docs with swagger

--- account-api/src/routes/routes.js
+++ account-api/src/routes/routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const indexRoute = require('./index');
 const accountRoute = require('./account');
+const documentationRoute = require('./documentation');
 
 router.use('/', indexRoute);
 router.use('/account', accountRoute);
+router.use('/doc', documentationRoute);
 
 module.exports = router;
